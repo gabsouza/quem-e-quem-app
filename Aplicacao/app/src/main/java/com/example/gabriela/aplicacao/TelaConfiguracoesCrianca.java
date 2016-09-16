740b00fed812a2a46d432dcdaf2caf44e522ea5f
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-09-16 19:17:47 -0300

    add classe perfil, responsavel e selo

--- Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaConfiguracoesCrianca.java
+++ Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaConfiguracoesCrianca.java
@@ -1,10 +1,14 @@
 package com.example.gabriela.aplicacao;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -14,15 +18,28 @@ public class TelaConfiguracoesCrianca extends Activity {
     private ViewPager viewPager;
     private CustomSwip customSwip;
     private EditText et_nome;
+    private ImageButton ib_camera;
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actitivity_configuracoes_crianca);
+
         inicializaComponentes();
+
         customSwip = new CustomSwip(this);
         viewPager.setAdapter(customSwip);
+
+        this.ib_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // editor.clear();
+               // editor.commit();
+               // startActivity(itTelaLogin);
+                finish();
+            }
+        });
     }
 
     /*private void toolbarsetters(){
@@ -37,5 +54,6 @@ public class TelaConfiguracoesCrianca extends Activity {
        // toolbar = (Toolbar) findViewById(R.id.tb_principal);
         viewPager = (ViewPager)findViewById(R.id.viewPager);
         et_nome = (EditText)findViewById(R.id.et_nome);
+        ib_camera = (ImageButton)findViewById(R.id.ib_camera);
     }
 }
---
+++ Aplicacao/app/src/main/java/pojo/Responsavel.java
@@ -0,0 +1,7 @@
+package pojo;
+
+/**
+ * Created by Gabriela on 16/09/2016.
+ */
+public class Responsavel {
+}
