79c632eee93f9ddb85426a094b11f38e4b070241
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-10-14 12:07:41 -0300

    adicionar responsansavelConsumer

---
+++ Aplicacao/app/src/main/java/consumer/ResponsavelConsumer.java
@@ -0,0 +1,44 @@
+package consumer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pojo.Responsavel;
+
+/**
+ * Created by Gabriela on 14/10/2016.
+ */
+
+public class ResponsavelConsumer {
+
+    RestTemplate restTemplate;
+    public static final String URL_BASE = "http://192.168.241.63:8080/ServidorQuem/rest/responsavel/";
+
+    public ResponsavelConsumer() {
+        restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+    }
+
+    public Responsavel chamaConsultarPorId(int id) {
+        String URL = URL_BASE+"/"+id;
+        Responsavel responsavel = restTemplate.getForObject(URL, Responsavel.class);
+        return responsavel;
+    }
+
+    public Responsavel chamaCadastrar(Responsavel responsavel) {
+        ResponseEntity<Responsavel> response = restTemplate.postForEntity(URL_BASE, responsavel, Responsavel.class);
+        responsavel = response.getBody();
+        return responsavel;
+    }
+
+    /*public void chamaDeletar(int id){
+        String URL = URL_BASE + "{id}";
+        Map map = new HashMap();
+        map.put("id", id);
+        restTemplate.delete(URL, map);
+    }*/
+}
