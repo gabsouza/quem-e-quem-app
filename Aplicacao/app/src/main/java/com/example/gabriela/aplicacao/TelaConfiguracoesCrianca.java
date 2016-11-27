bfd8e6eba6486b70b0fd24b0f1f005e336c9192a
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-11-27 03:56:46 -0200

    Adicionar personagens randomicamente

--- Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaConfiguracoesCrianca.java
+++ Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaConfiguracoesCrianca.java
@@ -88,14 +88,14 @@ public class TelaConfiguracoesCrianca extends Activity {
             }
         });
 
-      this.btAtualizar.setOnClickListener(new View.OnClickListener() {
+        btAtualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Perfil perfil = new Perfil();
-//                perfil.setNomePerfil(etNome.getText().toString());
-//                new HttpRequestTask().execute(perfil);
-//                Log.i("DEBUG",((CustomSwip)viewPager.getAdapter()).getImagemCorrente()+"");
-               // perfil.setMidia((CustomSwip) viewPager.getAdapter()).getImagemCorrente());
+                Perfil perfil = new Perfil();
+                perfil.setNomePerfil(etNomeAtualiza.getText().toString());
+               // new HttpRequestTask().execute(perfil);
+            // Log.i("DEBUG",((CustomSwip)viewPager.getAdapter()).setImagemCorrente()+"");
+               // perfil.setMidia((CustomSwip) viewPager.getAdapter()).setImagemCorrente());
                 Toast.makeText(TelaConfiguracoesCrianca.this, "Atualizado", Toast.LENGTH_LONG).show();
 
                 chamaTelaMain();
@@ -163,12 +163,9 @@ public class TelaConfiguracoesCrianca extends Activity {
 
     public void playAudio(Uri uri){
         try {
-
-
                 mp.setDataSource(this, uri);
                 mp.prepare();
                 mp.start();
-
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -217,7 +214,6 @@ public class TelaConfiguracoesCrianca extends Activity {
                     }
                 }
                 break;
-
         }
     }
 
