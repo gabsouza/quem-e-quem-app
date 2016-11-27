6b4447c1fda0798143f178c2cc21d2afae9321af
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-11-27 04:54:19 -0200

    Adicionar TextView para o jogo

--- Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoProfissao.java
+++ Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoProfissao.java
@@ -6,6 +6,7 @@ import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -16,6 +17,7 @@ public class TelaJogoProfissao extends Activity{
 
     private ImageView ivPersonagem;
     private Button btPassar;
+    private TextView pergunta, opcao1, opcao2, opcao3, opcao4, opcao5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +28,22 @@ public class TelaJogoProfissao extends Activity{
         btPassar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int[] cards={R.drawable.personagem1,R.drawable.personagem2,R.drawable.personagem3,R.drawable.personagem4,R.drawable.personagem5,R.drawable.personagem6,R.drawable.personagem7};
+                int[] cards={R.drawable.personagem1,R.drawable.personagem2,R.drawable.personagem3,R.drawable.personagem4,R.drawable.personagem5,R.drawable.personagem6,R.drawable.personagem7, R.drawable.personagem8, R.drawable.personagem9};
                 Random r = new Random();
                 int n=r.nextInt(7);
                 ivPersonagem.setImageResource(cards[n]);
             }
         });
-
     }
 
     public void inicializaComponentes(){
         ivPersonagem = (ImageView) findViewById(R.id.iv_personagem);
         btPassar = (Button) findViewById(R.id.bt_randon);
+        pergunta = (TextView) findViewById(R.id.tv_pergunta);
+        opcao1 = (TextView) findViewById(R.id.tv_opcao1);
+        opcao2 = (TextView) findViewById(R.id.tv_opcao2);
+        opcao3 = (TextView) findViewById(R.id.tv_opcao3);
+        opcao4 = (TextView) findViewById(R.id.tv_opcao4);
+        opcao5 = (TextView) findViewById(R.id.tv_opcao5);
     }
 }
