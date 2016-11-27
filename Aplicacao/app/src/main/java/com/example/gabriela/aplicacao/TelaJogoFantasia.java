ff62773ce2e7e80d0b1e83bf9bd5185730b8fde1
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-11-27 00:15:14 -0200

    Adicionar activity TelaJogoFantasia

--- Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoFantasia.java
+++ Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoFantasia.java
@@ -3,14 +3,17 @@ package com.example.gabriela.aplicacao;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.gabriela.aplicacao.R;
+
 /**
- * Created by Gabriela on 18/11/2016.
+ * Created by Gabriela on 26/11/2016.
  */
-public class TelaJogoProfissao extends Activity{
+public class TelaJogoFantasia extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jogo_profissao);
+
+        setContentView(R.layout.activity_jogo_fantasia);
     }
 }
