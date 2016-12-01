4435925103deee964d20c9703c67334d9fa986b5
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-12-01 12:03:33 -0200

    Adionar perguntas randon

--- Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoProfissao.java
+++ Aplicacao/app/src/main/java/com/example/gabriela/aplicacao/TelaJogoProfissao.java
@@ -1,6 +1,7 @@
 package com.example.gabriela.aplicacao;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import consumer.PerguntaConsumer;
@@ -25,7 +29,8 @@ public class TelaJogoProfissao extends Activity {
     private TextView tvPergunta;
     private Button opcao1, opcao2, opcao3, opcao4, opcao5;
     private PerguntaConsumer perguntaConsumer;
-    private Pergunta pergunta, perg;
+    private Pergunta pergunta;
+    private List<Pergunta> perguntas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,47 +43,71 @@ public class TelaJogoProfissao extends Activity {
         btPassar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int[] cards = {R.drawable.personagem1, R.drawable.personagem2, R.drawable.personagem3, R.drawable.personagem4, R.drawable.personagem5, R.drawable.personagem6, R.drawable.personagem7, R.drawable.personagem8, R.drawable.personagem9};
-                Random r = new Random();
-                int n = r.nextInt(7);
-                ivPersonagem.setImageResource(cards[n]);
-
+                obterPersonagensAleatorios();
+                obterPerguntasAleatorias();
             }
         });
 
     }
 
+    public void obterPersonagensAleatorios(){
+        int[] cards = {R.drawable.personagem1, R.drawable.personagem2, R.drawable.personagem3, R.drawable.personagem4, R.drawable.personagem5, R.drawable.personagem6, R.drawable.personagem7, R.drawable.personagem8, R.drawable.personagem9};
+        Random r = new Random();
+        int n = r.nextInt(9);
+        ivPersonagem.setImageResource(cards[n]);
+    }
+
+    public void obterPerguntasAleatorias(){
+        Random ran = new Random();
+
+        if (perguntas.size() >0){
+
+        int ranNum = ran.nextInt(this.perguntas.size());
+        tvPergunta.setText(perguntas.get(ranNum).getDescricao());
+        perguntas.remove(ranNum);
+
+        }else{
+            chamaTelaResultado();
+        }
+    }
+
+    public void chamaTelaResultado(){
+        Intent itTelaResultado = new Intent(this, TelaResultado.class);
+        startActivity(itTelaResultado);
+        finish();
+    }
+
     public void inicializaComponentes(){
         ivPersonagem = (ImageView) findViewById(R.id.iv_personagem);
-        btPassar = (Button) findViewById(R.id.bt_randon);
+
         tvPergunta = (TextView) findViewById(R.id.tv_pergunta);
+
+        btPassar = (Button) findViewById(R.id.bt_randon);
         opcao1 = (Button) findViewById(R.id.tv_opcao1);
         opcao2 = (Button) findViewById(R.id.tv_opcao2);
         opcao3 = (Button) findViewById(R.id.tv_opcao3);
         opcao4 = (Button) findViewById(R.id.tv_opcao4);
         opcao5 = (Button) findViewById(R.id.tv_opcao5);
+
         perguntaConsumer = new PerguntaConsumer();
         pergunta = new Pergunta();
+        perguntas = new ArrayList<Pergunta>();
     }
 
-    private class HttpRequestTask extends AsyncTask<Void, Void, Pergunta> {
+    private class HttpRequestTask extends AsyncTask<Void, Void, List<Pergunta>> {
 
         // EXECUTA A TAREFA QUE DEVE SER REALIZADA
         @Override
-
-        protected Pergunta doInBackground(Void... params) {
-            pergunta = perguntaConsumer.chamaConsultarPorId(5);
-            return pergunta;
+        protected List<Pergunta> doInBackground(Void... params) {
+           perguntas = perguntaConsumer.chamaListar();
+            return perguntas;
         }
 
         // é executado quando o webservice retorna
         @Override
-        protected void onPostExecute(Pergunta perg) {
-            super.onPostExecute(pergunta);
-            Log.i("DEBUG",pergunta.getDescricao());
-            pergunta = perg;
-            tvPergunta.setText(pergunta.getDescricao());
+        protected void onPostExecute(List<Pergunta> pergs) {
+            super.onPostExecute(perguntas);
+            perguntas = pergs;
         }
     }
-
 }
