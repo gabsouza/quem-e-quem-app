4af26c26c4b9b940940f9c5795897f268bb42ee6
GAB-PC\Gabriela <gabsouza98@gmail.com>
2016-12-11 20:25:07 -0200

    corrigir erro

--- Aplicacao/app/src/main/java/consumer/AlternativaConsumer.java
+++ Aplicacao/app/src/main/java/consumer/AlternativaConsumer.java
@@ -17,7 +17,7 @@ public class AlternativaConsumer {
 
     RestTemplate restTemplate;
 
-    public static final String URL_BASE = "http://192.168.1.5:8080/ServidorQuem/rest/alternativa/";
+    public static final String URL_BASE = "http://192.168.0.105:8080/ServidorQuem/rest/alternativa/";
 
     public AlternativaConsumer() {
         restTemplate = new RestTemplate();
